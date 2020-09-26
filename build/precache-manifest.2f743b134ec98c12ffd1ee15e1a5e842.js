d7b1bd46859364fa64461d1c62ec35c6ecbbff4b
Yanislav Zagorov <yanislav@mnemonic.no>
2020-09-27 00:42:22 +0200

    Pushing production build manually

---
+++ build/precache-manifest.2f743b134ec98c12ffd1ee15e1a5e842.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "014418063318f65a2a42af08f6b2ec98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56b2e0e16596c8f73b5d",
+    "url": "/static/css/main.c424a580.chunk.css"
+  },
+  {
+    "revision": "e391dcee2f43900c40ec",
+    "url": "/static/js/2.56141fd1.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/static/js/2.56141fd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56b2e0e16596c8f73b5d",
+    "url": "/static/js/main.2c2078cf.chunk.js"
+  },
+  {
+    "revision": "7a82cd163df1648f0072",
+    "url": "/static/js/runtime-main.67b8b2f6.js"
+  }
+]);
